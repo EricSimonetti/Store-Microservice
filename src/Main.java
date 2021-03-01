aa7736200c7c16bc5f1c5742d446142bf1027fe4
EricSimonetti <56443205+EricSimonetti@users.noreply.github.com>
2021-03-01 13:39:12 -0500

    implemented dijkstra

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,5 +10,80 @@ public class Main {
 
     private void createGraph(Node node){
 
+    }
+
+    private ArrayList<Node> findShortishPath(Node entrance, ArrayList<String> items){
+        ArrayList<Node> shortishPath = new ArrayList<>();
+        Node source = entrance;
+
+        while(items.size()>0){
+            shortishPath.addAll(dijkstra(source, items));      //do dijkstra to find next closest required node
+            source = shortishPath.get(shortishPath.size()-1);  //concat path to next node
+            resetDijkstra(entrance);                           //reset values and parents
+        }
+
+        return shortishPath;
+    }
+
+    private ArrayList<Node> dijkstra(Node source, ArrayList<String> items){
+        Node nextClosest = new Node(0, "", null, null);
+        ArrayList<Node> queue = new ArrayList<>();
+
+        queue.add(source);
+        source.setPathLength(0);
+        Node current;
+        while(queue.size()>0){
+            current = queue.get(0);
+            queue.remove(current);
+
+            for(Edge e : current.getNeighbors()){
+                Node edgeNode = e.getNode();
+                double newPathLength = e.getWeight() + current.getPathLength();
+
+                if(newPathLength < edgeNode.getPathLength()){
+                    edgeNode.setPathLength(newPathLength);
+                    edgeNode.setParent(current);
+                }
+                if(current.getPathLength() < nextClosest.getPathLength()){
+                    queue.add(current);
+                    if(current.getLeftItems().stream().anyMatch(items::contains) ||  //if the node is a required node
+                       current.getRightItems().stream().anyMatch(items::contains)){
+                        nextClosest = current;                                       //set that node as nextClosest
+                        items.removeAll(current.getLeftItems().stream()              //remove items in that node from the
+                                .filter(items::contains)                             //set we're searching for
+                                .collect(Collectors.toList()));
+                        items.removeAll(current.getRightItems().stream()
+                                .filter(items::contains)
+                                .collect(Collectors.toList()));
+                    }
+                }
+            }
+        }
+        ArrayList<Node> nextClosestPath = new ArrayList<>();
+        current = nextClosest;
+        while(current.getParent()!=null){
+            nextClosestPath.add(0, current);
+        }
+        return nextClosestPath;
+    }
+
+    private void resetDijkstra(Node source){
+        ArrayList<Node> queue = new ArrayList<>();
+        Node current = source;
+        queue.add(current);
+        while(queue.size()>0) {
+            current = queue.get(0);
+            queue.remove(current);
+
+            for (Edge e : current.getNeighbors()) {
+                Node edgeNode = e.getNode();
+
+                if (edgeNode.getParent()!=null) {
+                    edgeNode.setParent(null);
+                    edgeNode.setPathLength(Double.MAX_VALUE);
+                    queue.add(edgeNode);
+                }
+            }
+        }
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -3,16 +3,19 @@ import java.util.ArrayList;
 public class Node {
     private int id;
     private String locationFlag;
-    private String[] rightItems;
-    private String[] leftItems;
+    private ArrayList<String> rightItems;
+    private ArrayList<String> leftItems;
     private ArrayList<Edge> neighbors;
+    private double pathLength;
+    private Node parent;
 
-    public Node(int id, String locationFlag, String[] rightItems, String[] leftItems){
+    public Node(int id, String locationFlag, ArrayList<String> rightItems, ArrayList<String> leftItems){
         this.id = id;
         this.locationFlag = locationFlag;
         this.rightItems = rightItems;
         this.leftItems = leftItems;
         neighbors = new ArrayList<>();
+        pathLength = Double.MAX_VALUE;
     }
 
     public void addNeighbor(Node neighbor, double weight){
@@ -31,11 +34,27 @@ public class Node {
         return neighbors;
     }
 
-    public String[] getLeftItems() {
+    public ArrayList<String> getLeftItems() {
         return leftItems;
     }
 
-    public String[] getRightItems() {
+    public ArrayList<String> getRightItems() {
         return rightItems;
+    }
+
+    public double getPathLength() {
+        return pathLength;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setPathLength(double pathLength) {
+        this.pathLength = pathLength;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
     }
 }
