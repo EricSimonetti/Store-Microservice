518ba28c6151fef52ead182c07d4beb4384aa986
EricSimonetti <56443205+EricSimonetti@users.noreply.github.com>
2021-03-02 12:33:32 -0500

    fixed reset dijkstra

--- src/Main.java
+++ src/Main.java
@@ -17,9 +17,8 @@ public class Main {
         Node source = entrance;
 
         while(items.size()>0){
-            shortishPath.addAll(dijkstra(source, items));      //do dijkstra to find next closest required node
-            source = shortishPath.get(shortishPath.size()-1);  //concat path to next node
-            resetDijkstra(entrance);                           //reset values and parents
+            shortishPath.addAll(dijkstra(source, items));      //do dijkstra to find next closest required node + concat path to next node
+            source = shortishPath.get(shortishPath.size()-1);  //set source to the next closest required node
         }
 
         return shortishPath;
@@ -33,8 +32,7 @@ public class Main {
         source.setPathLength(0);
         Node current;
         while(queue.size()>0){
-            current = queue.get(0);
-            queue.remove(current);
+            current = queue.remove(0);
 
             for(Edge e : current.getNeighbors()){
                 Node edgeNode = e.getNode();
@@ -44,17 +42,11 @@ public class Main {
                     edgeNode.setPathLength(newPathLength);
                     edgeNode.setParent(current);
                 }
-                if(current.getPathLength() < nextClosest.getPathLength()){
+                if(current.getPathLength() < nextClosest.getPathLength()){             //if the node is closer than the closest required node
                     queue.add(current);
-                    if(current.getLeftItems().stream().anyMatch(items::contains) ||  //if the node is a required node
+                    if(current.getLeftItems().stream().anyMatch(items::contains) ||    //if the node is a required node
                        current.getRightItems().stream().anyMatch(items::contains)){
-                        nextClosest = current;                                       //set that node as nextClosest
-                        items.removeAll(current.getLeftItems().stream()              //remove items in that node from the
-                                .filter(items::contains)                             //set we're searching for
-                                .collect(Collectors.toList()));
-                        items.removeAll(current.getRightItems().stream()
-                                .filter(items::contains)
-                                .collect(Collectors.toList()));
+                        nextClosest = current;                                         //set that node as nextClosest
                     }
                 }
             }
@@ -65,6 +57,15 @@ public class Main {
             nextClosestPath.add(0, current);
             current = current.getParent();
         }
+
+        items.removeAll(nextClosest.getLeftItems().stream()   //remove matching items in nextClosest from the
+                .filter(items::contains)                      //set we're searching for
+                .collect(Collectors.toList()));
+        items.removeAll(nextClosest.getRightItems().stream()
+                .filter(items::contains)
+                .collect(Collectors.toList()));
+
+        resetDijkstra(source);                                //reset values and parents
         return nextClosestPath;
     }
 
